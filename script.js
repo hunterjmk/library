59f49eff9dbb090a022f06a137870603b8bbfb11
hunterjmk <136958839+hunterjmk@users.noreply.github.com>
2024-09-17 08:37:01 +0300

    Add the constructor function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+const mlibrary = [];
+
+function Book(title, author, numberOfPages, status) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.status = status;
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295 pages', 'Read');
+
+console.table(typeof theHobbit);
