36b651f1f5053d9b4a292ebb6a2aa2cbafbe5c48
hunterjmk <136958839+hunterjmk@users.noreply.github.com>
2024-09-17 10:46:05 +0300

    Add a dialog Modal
    The modal is fully styled. User must click new book button to make it appear. They can also close by clicking cancel.

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function Book(title, author, numberOfPages, status) {
 // const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295 pages', 'Read');
 
 // console.table(typeof theHobbit);
+
+const addBtn = document.querySelector('.new-book');
+const submitBtn = document.querySelector('.submit');
+const cancelBtn = document.querySelector('.cancel');
+const modal = document.querySelector('#dialog');
+
+addBtn.addEventListener('click', () => {
+    modal.showModal();
+});
+
+cancelBtn.addEventListener('click', () => {
+    modal.close();
+})
